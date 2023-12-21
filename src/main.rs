9d408147dfdac7b503332a0982d7a4ac7316673a
BoltR6 <johnwallacewhite@gmail.com>
2023-12-21 12:53:35 -0600

    Functional server

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::fs::{ OpenOptions, File };
-use std::io::{ Read };
 use std::env;
 use std::sync::{ Arc };
 use tokio::sync::Mutex;
 use serde::{ Serialize, Deserialize };
-use std::net::TcpListener;
+use std::{
+    io::{ Read, prelude::*, BufReader },
+    net::{TcpListener, TcpStream},
+};
 
 mod openai;
 mod memory;
@@ -114,10 +116,37 @@ async fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     for stream in listener.incoming() {
         let stream = stream.unwrap();
+
+        handle_connection(stream);
         println!("Connection established!");
     }
 }
 
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+    println!("{:?}", http_request);
+
+    let request_type = &http_request[0];
+
+    if request_type == "GET / HTTP/1.1" {
+        let status_line = "HTTP/1.1 200 OK";
+    
+        let contents = std::fs::read_to_string("client/hello.html").unwrap();
+    
+        let length = contents.len();
+    
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    
+        stream.write_all(response.as_bytes()).unwrap();
+    } else {
+        println!("Bad request!");
+    }
+}
 
 
 
